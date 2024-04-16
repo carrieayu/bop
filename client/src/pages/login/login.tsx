02d62cd1781220800b6054e7d2e195f354bf97c3
ian-bpoc <ian@bpoc.co.jp>
2024-04-16 15:13:02 +0800

    created loginUI

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm } from "./login.component";
-
+import './login.scss';
+import dattLogo from  '../../assets/logo.png'
+import {FaUser, FaEye} from 'react-icons/fa';
 interface LoginFormProps {
   onSubmit: (data: LoginData) => void;
 }
@@ -17,31 +19,85 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   });
 
   return (
-    <form onSubmit={(e) => handleSubmit(onSubmit, e)}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={values.username}
-          onChange={handleChange}
-        />
-        {errors.username && <p className="error">{errors.username}</p>}
+    <div className="container">
+      <div className="card-box">
+        <div className="card-content">
+          <div className="left-content">
+              <div className="logo">
+                  <img src={dattLogo} alt="Datt Japan Logo" />
+              </div>
+          </div>
+
+          <div className="line">
+            <div className="line-content-a"></div>
+            <div className="line-content"></div>
+          </div>
+
+          <div className="right-content">
+            <div className="form">
+              <form action="">
+              <div className="field">
+                    <p className="control">
+                      <label htmlFor="username" className="label">Username</label>
+                      <input type="text"className="input"
+                        placeholder="Enter username..."
+                        style={{ fontFamily: 'Arial, FontAwesome' }} // Set font family to FontAwesome
+                      />
+                      <span className="icon is-medium is-right">
+                        <FaUser />
+                      </span>
+                    </p>
+                  </div>
+                  <div className="field">
+                    <p className="control">
+                      <label htmlFor="password" className="label">Password</label>
+                      <input type="password" className="input" placeholder="*******"/>
+                      <span className="icon is-medium is-right">
+                        <FaEye />
+                      </span>
+                    </p>
+                  </div>
+                  <div className="field">
+                    <p className="control has-text-centered">
+                      <button className="button">
+                        Login
+                      </button>
+                      <div className="link">
+                        <a href="#">No account? Sign up here!</a>
+                      </div>
+                    </p>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-        />
-        {errors.password && <p className="error">{errors.password}</p>}
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    </div>
+    // <form onSubmit={(e) => handleSubmit(onSubmit, e)}>
+    //   <div>
+    //     <label htmlFor="username">Username:</label>
+    //     <input
+    //       type="text"
+    //       id="username"
+    //       name="username"
+    //       value={values.username}
+    //       onChange={handleChange}
+    //     />
+    //     {errors.username && <p className="error">{errors.username}</p>}
+    //   </div>
+    //   <div>
+    //     <label htmlFor="password">Password:</label>
+    //     <input
+    //       type="password"
+    //       id="password"
+    //       name="password"
+    //       value={values.password}
+    //       onChange={handleChange}
+    //     />
+    //     {errors.password && <p className="error">{errors.password}</p>}
+    //   </div>
+    //   <button type="submit">Login</button>
+    // </form>
   );
 };
 
