4728d659b27b2d44888991e0ca991196a68e85b2
Carre Yu <carrie@bpoc.co.jp>
2024-04-18 10:51:53 +0800

    Test Updates and prettiers Updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
-import './assets/scss/main.scss'
 
 function App() {
   const content = useRoutes(routes)
