c88e0e6739e18f2f14b9ac3439459336f6c431be
ian-bpoc <ian@bpoc.co.jp>
2024-04-18 10:40:29 +0800

    Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import routes from "./routes";
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
 import './assets/scss/main.scss'
 
 function App() {
-  const content = useRoutes(routes);
+  const content = useRoutes(routes)
 
-  return <div className="App">{content}</div>;
+  return <div className='App'>{content}</div>
 }
 
-export default App;
+export default App
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import "bulma/css/bulma.min.css";
+import React from 'react'
+import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import 'bulma/css/bulma.min.css'
 
 // import dotenv from "dotenv";
 
 // dotenv.config();
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -25,4 +23,4 @@ root.render(
       </HelmetProvider>
     </Provider>
   </React.StrictMode>,
-);
+)
