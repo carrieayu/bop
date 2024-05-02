f590214e9b0da8ce5fa1f1f642392414865793be
merlouduron <102946511+merlouduron@users.noreply.github.com>
2024-05-02 13:02:45 +0800

    reuseable header component (#5)
    * reuseable header component
    
    * remove header component
    
    ---------
    
    Co-authored-by: carrieayu <105268667+carrieayu@users.noreply.github.com>

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "./login.component";
 import dattLogo from  '../../assets/images/logo.png'
 import {FaUser, FaEye} from 'react-icons/fa';
+import { HeaderDashboard } from "../../components/header/header";
 interface LoginFormProps {
   onSubmit: (data: LoginData) => void;
 }
@@ -18,61 +19,65 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   });
 
   return (
-    <div className="container">
-      <div className="card-box">
-        <div className="card-content">
-          <div className="left-content">
-              <div className="logo">
-                  <img src={dattLogo} alt="Datt Japan Logo" />
+      <div className='container'>
+        <div className='card-box'>
+          <div className='card-content'>
+            <div className='left-content'>
+              <div className='logo'>
+                <img src={dattLogo} alt='Datt Japan Logo' />
               </div>
-          </div>
+            </div>
 
-          <div className="line">
-            <div className="line-content-a"></div>
-            <div className="line-content"></div>
-          </div>
+            <div className='line'>
+              <div className='line-content-a'></div>
+              <div className='line-content'></div>
+            </div>
 
-          <div className="right-content">
-            <div className="form">
-              <form onSubmit={(e) => handleSubmit(onSubmit, e)}>
-              <div className="field">
-                    <p className="control">
-                      <label htmlFor="username" className="label">Username</label>
-                      <input type="text"className="input"
-                        placeholder="Enter username..."
+            <div className='right-content'>
+              <div className='form'>
+                <form onSubmit={(e) => handleSubmit(onSubmit, e)}>
+                  <div className='field'>
+                    <p className='control'>
+                      <label htmlFor='username' className='label'>
+                        Username
+                      </label>
+                      <input
+                        type='text'
+                        className='input'
+                        placeholder='Enter username...'
                         style={{ fontFamily: 'Arial, FontAwesome' }}
                       />
-                      <span className="icon is-medium is-right">
+                      <span className='icon is-medium is-right'>
                         <FaUser />
                       </span>
                     </p>
                   </div>
-                  <div className="field">
-                    <p className="control">
-                      <label htmlFor="password" className="label">Password</label>
-                      <input type="password" className="input" placeholder="*******"/>
-                      <span className="icon is-medium is-right">
+                  <div className='field'>
+                    <p className='control'>
+                      <label htmlFor='password' className='label'>
+                        Password
+                      </label>
+                      <input type='password' className='input' placeholder='*******' />
+                      <span className='icon is-medium is-right'>
                         <FaEye />
                       </span>
                     </p>
                   </div>
-                  <div className="field">
-                    <p className="control has-text-centered">
-                      <button className="button">
-                        Login
-                      </button>
-                      <div className="link">
-                        <a href="#">No account? Sign up here!</a>
+                  <div className='field'>
+                    <p className='control has-text-centered'>
+                      <button className='button'>Login</button>
+                      <div className='link'>
+                        <a href='#'>No account? Sign up here!</a>
                       </div>
                     </p>
                   </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+  )
 };
 
 export default LoginForm;
