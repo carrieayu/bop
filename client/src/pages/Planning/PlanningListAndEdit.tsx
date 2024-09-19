c8f778b55ef617ccacff97145a63a78a2a899884
JoshM0101 <joshua@bpoc.co.jp>
2024-09-19 13:31:38 +0800

    refactor: change the register and sidebar to temporary filenames

--- client/src/pages/Planning/PlanningListAndEdit.tsx
+++ client/src/pages/Planning/PlanningListAndEdit.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { UnknownAction } from '@reduxjs/toolkit';
 import { fetchAllClientData } from '../../reducers/table/tableSlice';
-import Sidebar from '../../components/Sidebar/Sidebar';
+import Sidebar from '../../components/temp_SideBar/Sidebar';
 import Btn from '../../components/Button/Button';
 import { useLocation, useNavigate } from 'react-router-dom';
 import TablePlanningA from '../../components/Table/TablePlanningA'; 
