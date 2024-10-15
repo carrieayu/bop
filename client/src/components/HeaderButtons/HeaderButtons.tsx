4aebca3fbd4ec9c2cc9b12416a2f5c370032de20
merlouduron <merlouduron@bpoc.co.jp>
2024-10-15 16:20:36 +0800

    implemented logout function

--- client/src/components/HeaderButtons/HeaderButtons.tsx
+++ client/src/components/HeaderButtons/HeaderButtons.tsx
@@ -1,9 +1,10 @@
 // src/components/HeaderButtons/HeaderButtons.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Btn from '../Button/Button'; 
 import { translate } from '../../utils/translationUtil';
 import { useLanguage } from '../../contexts/LanguageContext';
-
+import { TbLogout } from 'react-icons/tb'
+import AlertModal from '../AlertModal/AlertModal';
 interface HeaderButtonsProps {
   activeTab: string;
   handleTabClick: (tab: string) => void;
@@ -11,37 +12,66 @@ interface HeaderButtonsProps {
   handleTranslationSwitchToggle: () => void;
 }
 
+
+
 const HeaderButtons: React.FC<HeaderButtonsProps> = ({ activeTab, handleTabClick, isTranslateSwitchActive, handleTranslationSwitchToggle }) => {
   const { language } = useLanguage();
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
+
+
+  const handleConfirm = async () => {
+      localStorage.removeItem('accessToken')
+      const token = localStorage.getItem('accessToken')
+      if (!token) {
+        window.location.href = '/login' 
+        return
+      }
+      setModalIsOpen(false)
+    }
 
   return (
     <div className='HeaderButtons_header_cont'>
-      <div className="HeaderButtons_header-buttons">
+      <div className='HeaderButtons_header-buttons'>
         <Btn
           label={translate('analysis', language)}
-          onClick={() => handleTabClick("/dashboard")}
-          className={activeTab === "/dashboard" ? "h-btn-active header-btn" : "header-btn"}
+          onClick={() => handleTabClick('/dashboard')}
+          className={activeTab === '/dashboard' ? 'h-btn-active header-btn' : 'header-btn'}
         />
         <Btn
           label={translate('profitAndlossPlanning', language)}
-          onClick={() => handleTabClick("/planning-list")}
-          className={activeTab === "/planning-list" ? "h-btn-active header-btn" : "header-btn"}
+          onClick={() => handleTabClick('/planning-list')}
+          className={activeTab === '/planning-list' ? 'h-btn-active header-btn' : 'header-btn'}
         />
         <Btn
           label={translate('results', language)}
-          onClick={() => handleTabClick("/*")}
-          className={activeTab === "/*" ? "h-btn-active header-btn" : "header-btn"}
+          onClick={() => handleTabClick('/*')}
+          className={activeTab === '/*' ? 'h-btn-active header-btn' : 'header-btn'}
         />
       </div>
-      <div className="HeaderButtons_language-toggle">
-        <p className="HeaderButtons_pl-label">English</p>
-        <label className="HeaderButtons_switch">
-          <input type="checkbox" checked={isTranslateSwitchActive} onChange={handleTranslationSwitchToggle} />
-          <span className="HeaderButtons_slider"></span>
+      <div className='HeaderButtons_language-toggle'>
+        <p className='HeaderButtons_pl-label'>English</p>
+        <label className='HeaderButtons_switch'>
+          <input type='checkbox' checked={isTranslateSwitchActive} onChange={handleTranslationSwitchToggle} />
+          <span className='HeaderButtons_slider'></span>
         </label>
+        <TbLogout className='logout-icon' onClick={() => openModal()} />
       </div>
+      <AlertModal
+        isOpen={modalIsOpen}
+        onConfirm={handleConfirm}
+        onCancel={closeModal}
+        message={translate('logout', language)}
+      />
     </div>
-  );
+  )
 }
 
 export default HeaderButtons;
