0cc2bb371510d3500771cf89c71aa4842b5581ad
merlouduron <merlouduron@bpoc.co.jp>
2024-10-28 14:10:57 +0800

    updated graph calculation

--- client/src/reducers/graph/graphSlice.ts
+++ client/src/reducers/graph/graphSlice.ts
@@ -25,11 +25,29 @@ const initialState: GraphDataState = {
   month: [],
 }
 
+const calculateGrossProfit = (card) => Number(card.sales_revenue) - Number(card.cost_of_sale)
+const calculateGrossProfitMargin = (grossProfit, salesRevenue) =>
+  salesRevenue ? (grossProfit / salesRevenue) * 100 : 0
+const calculateCumulativeOrdinaryIncome = (card) =>
+  Number(card.operating_profit) + Number(card.non_operating_profit) - Number(card.non_operating_expense)
+const calculateOperatingProfitMargin = (operatingProfit, salesRevenue) =>
+  salesRevenue ? (operatingProfit / salesRevenue) * 100 : 0
+const calculateOperatingIncome = (card) => {
+  const salesRevenue = Number(card.sales_revenue) || 0
+  const costOfSale = Number(card.cost_of_sale) || 0
+  const dispatchLaborExpense = Number(card.dispatch_labor_expense) || 0
+  const employeeExpense = Number(card.employee_expense) || 0
+  const indirectEmployeeExpense = Number(card.indirect_employee_expense) || 0
+  const otherExpense = Number(card.expense) || 0
+
+  return salesRevenue - costOfSale - dispatchLaborExpense - employeeExpense - indirectEmployeeExpense - otherExpense
+}
+
+
 export const fetchGraphData = createAsyncThunk('graphData/fetch', async () => {
   const response = await api.get<CardEntity[]>(`http://127.0.0.1:8000/api/projects/`)
   // const response = await api.get<CardEntity[]>(`http://54.178.202.58:8000/api/projects/`)
   const cards = response.data.map((data) => new CardEntity(data))
-  
   const aggregatedData: Partial<GraphDataState> = {
     totalSalesByDate: {},
     totalOperatingProfitByDate: {},
@@ -42,6 +60,12 @@ export const fetchGraphData = createAsyncThunk('graphData/fetch', async () => {
 
   cards.forEach((card) => {
     const date = `${card.year}-${card.month}`
+    const operatingIncome = calculateOperatingIncome(card)
+    const grossProfit = calculateGrossProfit(card)
+
+    const cumulataiveOridinaryIncome = calculateCumulativeOrdinaryIncome(card)
+    const grossProfitMargin = calculateGrossProfitMargin(calculateGrossProfit(card), card.sales_revenue)
+    const operatingProfitMargin = calculateOperatingProfitMargin(Number(card.operating_profit), card.sales_revenue)
 
     if (!aggregatedData.totalSalesByDate![date]) {
       aggregatedData.totalSalesByDate![date] = 0
@@ -53,14 +77,14 @@ export const fetchGraphData = createAsyncThunk('graphData/fetch', async () => {
     }
 
     aggregatedData.totalSalesByDate![date] += parseFloat(card.sales_revenue?.toString() || '0')
-    aggregatedData.totalOperatingProfitByDate![date] += parseFloat(card.operating_profit?.toString() || '0')
-    aggregatedData.totalGrossProfitByDate![date] += parseFloat(card.ordinary_income?.toString() || '0')
-    aggregatedData.totalNetProfitPeriodByDate![date] += parseFloat(card.non_operating_profit?.toString() || '0')
-    aggregatedData.totalGrossProfitMarginByDate![date] += parseFloat(card.ordinary_profit_margin?.toString() || '0')
-    aggregatedData.totalOperatingProfitMarginByDate![date] += parseFloat(card.non_operating_expense?.toString() || '0',)
+    aggregatedData.totalOperatingProfitByDate![date] += parseFloat(operatingIncome?.toString() || '0')
+    aggregatedData.totalGrossProfitByDate![date] += parseFloat(grossProfit?.toString() || '0')
+
+    aggregatedData.totalNetProfitPeriodByDate![date] += parseFloat(cumulataiveOridinaryIncome?.toString() || '0')
+    aggregatedData.totalGrossProfitMarginByDate![date] += parseFloat(grossProfitMargin?.toString() || '0')
+    aggregatedData.totalOperatingProfitMarginByDate![date] += parseFloat(operatingProfitMargin?.toString() || '0')
     aggregatedData.month.push(date)
   })
-  console.log(aggregatedData)
   return aggregatedData
 })
 
