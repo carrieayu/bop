ed3fed95b453e672f384f38e00adbf458c495a7d
Edward Stalley <stalley.e@impl.co.jp>
2024-12-04 15:47:59 +0900

    Change Endpoint back to Local

--- client/src/toggleEndpoint.ts
+++ client/src/toggleEndpoint.ts
@@ -1,6 +1,6 @@
 const local = 'http://127.0.0.1:8000'
 const live = 'http://54.178.202.58:8000'
 
-const REACT_APP_ACTIVE_ENDPOINT = live
+const REACT_APP_ACTIVE_ENDPOINT = local
 
 export const getReactActiveEndpoint = () => REACT_APP_ACTIVE_ENDPOINT
