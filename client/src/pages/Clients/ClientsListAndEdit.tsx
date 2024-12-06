de170dfc4831fff637b9294d51d3e66ff85ac515
kuramatsu <takuyakuramatsu@datt.co.jp>
2024-12-06 04:49:57 +0000

    test commit2 BOP_MANAGEMENT_SYSTEM-160

--- client/src/pages/Clients/ClientsListAndEdit.tsx
+++ client/src/pages/Clients/ClientsListAndEdit.tsx
@@ -25,6 +25,7 @@ import { formatDate } from '../../utils/helperFunctionsUtil'
 import { getUser } from "../../api/UserEndpoint/GetUser";
 
 
+
 const ClientsListAndEdit: React.FC = () => {
     const [activeTab, setActiveTab] = useState('/planning-list')
     const navigate = useNavigate()
