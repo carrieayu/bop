f82d90693a28aedf26a8713643622da0664c84cf
kuramatsu <takuyakuramatsu@datt.co.jp>
2025-01-21 16:49:37 +0900

    タスク178 Fix: Year / Pulldowns (追加コミット「変数名称の整備」)

--- client/src/pages/CostOfSalesResults/CostOfSalesResultsListAndEdit.tsx
+++ client/src/pages/CostOfSalesResults/CostOfSalesResultsListAndEdit.tsx
@@ -495,7 +495,7 @@ const CostOfSalesResultsList: React.FC = () => {
                         </thead>
                         <tbody className='costOfSalesResultsList_table_body'>
                           {combinedData.map((costOfSale, index) => {
-                            const isLastExpenseOfYear = costOfSale.month === 3;
+                            const isLastCostOfSaleOfYear = costOfSale.month === 3;
                             const isEditable = costOfSale.cost_of_sale_result_id !== null
 
                             return (
@@ -591,7 +591,7 @@ const CostOfSalesResultsList: React.FC = () => {
                                     </td>
                                   </tr>
                                 ) : null}
-                                {isLastExpenseOfYear && (
+                                {isLastCostOfSaleOfYear && (
                                   <tr className='year-separator'>
                                     <td className='horizontal-line-cell' colSpan={9}>
                                       <div className='horizontal-line' />
@@ -639,7 +639,7 @@ const CostOfSalesResultsList: React.FC = () => {
                       </thead>
                       <tbody className='costOfSalesResultsList_table_body'>
                         {combinedData.map((costOfSale, index) => {
-                          const isLastExpenseOfYear = costOfSale.month === 3;
+                          const isLastCostOfSaleOfYear = costOfSale.month === 3;
                           return (
                             <React.Fragment key={index}>
                               <tr className='costOfSalesResultsList_table_body_content_horizontal'>
@@ -671,7 +671,7 @@ const CostOfSalesResultsList: React.FC = () => {
                                   {formatNumberWithCommas(costOfSale.amortization_expense) || 0}
                                 </td>
                               </tr>
-                              {isLastExpenseOfYear && (
+                              {isLastCostOfSaleOfYear && (
                                 <tr className='year-separator'>
                                   <td className='horizontal-line-cell' colSpan={9}>
                                     <div className='horizontal-line' />
